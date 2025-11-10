ba37456b08c3c0d07070969273a6b05278dc3da3
CALDWELL-NICHOLS <scott.caldwell-nichols1@nhs.net>
2025-11-10 14:55:49 +0000

    basic config file for the repo

---
+++ src/config.py
@@ -0,0 +1,7 @@
+import pathlib
+
+base_dir = pathlib.Path('.')
+data_dir = base_dir / 'data'
+
+# GP catchment area data URL
+gp_catchment_area_url = 'https://www.healthgis.nhs.uk/assets/shared/GP_catchments_data.zip'
