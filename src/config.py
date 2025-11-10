f3d4ed0f352e6693da85b80663a8116146ca62b0
CALDWELL-NICHOLS <scott.caldwell-nichols1@nhs.net>
2025-11-10 16:12:23 +0000

    adds output dir and ods api url

--- src/config.py
+++ src/config.py
@@ -2,6 +2,10 @@ import pathlib
 
 base_dir = pathlib.Path('.')
 data_dir = base_dir / 'data'
+output_dir = base_dir / 'output'
 
 # GP catchment area data URL
 gp_catchment_area_url = 'https://www.healthgis.nhs.uk/assets/shared/GP_catchments_data.zip'
+
+# ODS API URL
+ods_api_url = 'https://directory.spineservices.nhs.uk/ORD/2-0-0/'
